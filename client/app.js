046308bf4ddb49f087f64c81bd90d986e1836397
Davor Grubelić <davor@uxpassion.com>
2016-08-10 12:56:37 +0200

    Removed angular app

--- client/app.js
+++ client/app.js
@@ -2,13 +2,3 @@
 
 // Load only used application components
 const indexComponent = require('./components/index');
-
-// Initialize application
-const app = angular.module('app', [
-  'index'
-]);
-
-app.config(require('./config'));
-app.run(function () {
-  
-});
--- client/components/index/index.js
+++ client/components/index/index.js
@@ -1,10 +1,1 @@
 'use strict';
-const indexModule = angular.module('index', []);
-
-indexModule.controller('IndexComponentController', require('./index.controller'));
-indexModule.component('indexComponent', {
-  templateUrl: 'views/index/index.html',
-  controller: 'IndexComponentController'
-});
-
-module.exports = indexModule;
